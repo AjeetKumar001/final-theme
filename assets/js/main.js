8998b626158c9dd26205686e127177951e214510
Ajeet Dhiman <ajeetd@pdcnew.iirs.gov.in>
2022-01-20 15:21:04 +0530

    sixth

--- assets/js/main.js
+++ assets/js/main.js
@@ -214,29 +214,29 @@
   });
 
   //Disable
-  // document.addEventListener("contextmenu", (event) => event.preventDefault());
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
 
-  // document.onkeydown = function (e) {
-  //   // disable F12 key
-  //   if (e.keyCode == 123) {
-  //     return false;
-  //   }
+  document.onkeydown = function (e) {
+    // disable F12 key
+    if (e.keyCode == 123) {
+      return false;
+    }
 
-  //   // disable I key
-  //   if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-  //     return false;
-  //   }
+    // disable I key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+      return false;
+    }
 
-  //   // disable J key
-  //   if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-  //     return false;
-  //   }
+    // disable J key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+      return false;
+    }
 
-  //   // disable U key
-  //   if (e.ctrlKey && e.keyCode == 85) {
-  //     return false;
-  //   }
-  // };
+    // disable U key
+    if (e.ctrlKey && e.keyCode == 85) {
+      return false;
+    }
+  };
   /* Instantiating iziModal */
   $("#modal-custom").iziModal({
     overlayClose: false,
